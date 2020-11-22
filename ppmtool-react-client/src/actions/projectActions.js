3dc7eecf9cc2528d27a276cde150c06878b13f7f
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-11-22 00:06:10 -0800

    task front-end added

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -5,6 +5,10 @@ export const createProject=(project,history)=>async dispatch =>{
 	try{
 		const res=await axios.post("http://localhost:8081/api/project",project);
 		history.push("/dashboard");
+		dispatch({
+			type:GET_ERRORS,
+			payload:{}
+		})
 	}catch(err)
 	{
 		dispatch({
@@ -25,11 +29,17 @@ export const getProjects=()=>async dispatch =>{
 }; 
 
 export const getProject = (id, history) => async dispatch => {
+	
+	try{
 	const res = await axios.get(`http://localhost:8081/api/project/${id}`);
 	dispatch({
 	  type: GET_PROJECT,
 	  payload: res.data
 	});
+	}catch(err)
+	{
+		history.push("/dashboard");
+	}
   };
 
 
